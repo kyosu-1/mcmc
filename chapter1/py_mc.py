6447755f228cf53b2e6ad9c3039bff1a63fc070b
kyosu-1 <sho013039@gmail.com>
2022-08-01 03:18:12 +0900

    change name

---
+++ chapter1/py_mc.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+# 半径1の円の面積をモンテカルロ法により求めることで、円周率の近似値を求める
+def py_mc(iter: int = 1000):
+    n_in = 0
+    for i in range(iter):
+        x, y = np.random.rand(), np.random.rand() # 0と1の間の一様乱数
+        if x**2 + y**2 < 1:
+            n_in += 1
+    return (n_in / iter) * 4
