8e97b8a7913c40cf6e72c61571730b76e75e4000
kyosu-1 <sho013039@gmail.com>
2022-08-01 03:33:37 +0900

    update

--- chapter1/py_mc.py
+++ chapter1/py_mc.py
@@ -5,7 +5,12 @@ import numpy as np
 def py_mc(iter: int = 1000):
     n_in = 0
     for i in range(iter):
-        x, y = np.random.rand(), np.random.rand() # 0と1の間の一様乱数
+        x, y = np.random.uniform(0, 1), np.random.uniform(0, 1) # 0と1の間の一様乱数
         if x**2 + y**2 < 1:
             n_in += 1
     return (n_in / iter) * 4
+
+
+if __name__ == '__main__':
+    print(py_mc(10000))
+    print(np.pi)
